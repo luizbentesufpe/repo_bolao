5461f3f0ca0e1536ce3df35ad23ab764eebc9607
luiz <lfmb2@cin.ufpe.br>
2026-06-11 03:07:00 -0400

    correct server.js

--- frontend/server.js
+++ frontend/server.js
@@ -3,13 +3,20 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const distPath = path.join(__dirname, 'dist', 'bolao');
+
+// Debug
+console.log(`Servindo arquivos de: ${distPath}`);
+console.log(`Arquivos existem: ${require('fs').existsSync(distPath)}`);
 
 // Servir arquivos estáticos
-app.use(express.static(path.join(__dirname, 'dist/bolao')));
+app.use(express.static(distPath));
 
 // Redirecionar todas as rotas para index.html (SPA)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/bolao', 'index.html'));
+  const indexPath = path.join(distPath, 'index.html');
+  console.log(`GET ${req.path} -> ${indexPath}`);
+  res.sendFile(indexPath);
 });
 
 app.listen(PORT, () => {
