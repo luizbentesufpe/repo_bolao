6f138c80c388d436d86037998431f0c02bb311ea
luiz <lfmb2@cin.ufpe.br>
2026-06-11 17:27:49 -0400

    add onDestroy

--- frontend/src/app/pages/bolao.component.ts
+++ frontend/src/app/pages/bolao.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DatePipe, CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../core/api.service';
