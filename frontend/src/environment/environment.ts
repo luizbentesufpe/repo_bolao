18371bf9ddb3f601b876e493701945258073da33
luiz <lfmb2@cin.ufpe.br>
2026-06-13 02:24:27 -0400

    new configs

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:5000',
-  vapidPublicKey: 'testando-chave-publica'  // Substitua pela sua chave pública VAPID do backend
+  production: true,
+  apiUrl: 'https://repo-bolao.onrender.com',  // sua URL do backend no Render,
+  vapidPublicKey: 'BC8EBHJPXTfR1JHQB2zRqQ731aqh0XlKalTRlKWbbFieoZDF6xioorcQAP7ysl4are3njDNwayqXGwvLE4m_87g'
 };
