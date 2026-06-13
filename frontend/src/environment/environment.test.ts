a2a005177edd5f7188f3ae1f43a565f17a2ddca9
luiz <lfmb2@cin.ufpe.br>
2026-06-13 16:28:12 -0400

    new enviroments

--- frontend/src/environment/environment.test.ts
+++ frontend/src/environment/environment.test.ts
@@ -1,7 +1,5 @@
-// frontend/src/environments/environment.test.ts
-
 export const environment = {
-  production: true,  // ✅ Config de PRODUÇÃO
-  apiUrl: 'https://test-backend-k1u1.onrender.com',  // ✅ Backend de TESTE
- vapidPublicKey: 'BC8EBHJPXTfR1JHQB2zRqQ731aqh0XlKalTRlKWbbFieoZDF6xioorcQAP7ysl4are3njDNwayqXGwvLE4m_87g'
+  production: true,
+  apiUrl: 'https://test-backend-k1u1.onrender.com',  // ← DEV backend
+  vapidPublicKey: 'BC8EBHJPXTfR1JHQB2zRqQ731aqh0XlKalTRlKWbbFieoZDF6xioorcQAP7ysl4are3njDNwayqXGwvLE4m_87g'
 };
--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://repo-bolao.onrender.com',  // sua URL do backend no Render,
+  production: false,
+  apiUrl: 'http://localhost:5000',  // sua URL do backend no Render,
   vapidPublicKey: 'BC8EBHJPXTfR1JHQB2zRqQ731aqh0XlKalTRlKWbbFieoZDF6xioorcQAP7ysl4are3njDNwayqXGwvLE4m_87g'
 };
